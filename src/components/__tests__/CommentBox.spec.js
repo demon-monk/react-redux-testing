cca0d33402a4575b7bfebfb904a6f800119dd703
Demon Monk <antdude@126.com>
2018-11-25 20:09:40 +0800

    use enzyme to test event handlers

--- src/components/__tests__/CommentBox.spec.js
+++ src/components/__tests__/CommentBox.spec.js
@@ -16,3 +16,12 @@ it('should has a textarea input and a submit button', () => {
     expect(wrapper.find('textarea').length).toEqual(1)
     expect(wrapper.find('button').length).toEqual(1)
 })
+
+it('should show the corrent value when use type into some text', () => {
+    const newCommentContent = 'new comment'
+    wrapper.find('textarea').simulate('change', {
+        target: { value: newCommentContent }
+    })
+    wrapper.update()
+    expect(wrapper.find('textarea').prop('value')).toEqual(newCommentContent)
+})
