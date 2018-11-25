9a60c526789e48d446480c43456c192d37298db3
Demon Monk <antdude@126.com>
2018-11-25 19:30:36 +0800

    add input and ui feature of commentBox component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 
-export default () => {
-    return (
-        <div>commentBox</div>
-    )
+export default class CommentBox extends PureComponent {
+
+    state = {
+        comment: ''
+    }
+
+    onTextAreaChange = (event) => {
+        this.setState({
+            comment: event.target.value
+        })
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    render () {
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <h4>add comment</h4>
+                <textarea value={this.state.comment} onChange={this.onTextAreaChange} />
+                <button>Submit comment</button>
+            </form>
+        )
+    }
 }
